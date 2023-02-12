42419998bdc785ad0420aa2eecbb16e2d085ea1c
musaib-js <musaibaltafurahman@gmail.com>
2023-02-12 10:04:10 +0530

    csrf admin commit

--- api/settings.py
+++ api/settings.py
@@ -11,12 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-<<<<<<< HEAD
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 CSRF_TRUSTED_ORIGINS = ['*.advaita-iiitbh.in']
-=======
-SECRET_KEY = str(os.getenv("SECRET_KEY"))
->>>>>>> 0524bb25f8967ac9cc863179529214f925e95d97
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
